01479fd2e516cce28c7d775f3b3e3b7c173a3d32
vinayakrkalburgi123 <rkalburgivinayak@gmail.com>
2026-07-11 22:43:51 +0530

    Fix static files

--- ai_trip_planner/settings.py
+++ ai_trip_planner/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,11 +126,13 @@ USE_TZ = True
 # STATIC FILES
 # ==========================
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'accounts' / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # ==========================
@@ -153,3 +156,5 @@ LOGOUT_REDIRECT_URL = "/login/"
 # ==========================
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
